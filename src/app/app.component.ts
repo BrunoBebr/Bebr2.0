1f698fd430192492fe0fb5d55646c45b6824482e
bruno <bebrbr20@sps-prosek.cz>
2023-05-17 16:36:15 +0200

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HostListener } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Bebr2.0';
+  Valheigt: any;
+
+  scrollToElement($element :any): void {
+    // console.log($element);
+     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+   }
+  
+  ngOnInit(){
+    this.Valheigt = window.innerHeight ;
+    
+     
+  }
 }
